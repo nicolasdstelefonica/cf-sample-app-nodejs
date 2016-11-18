8a4b277ae8bd5f7ca456147db83893b9a24a70c9
Juan Manuel Cruz <juan.cruzyanez@telefonica.com>
2016-11-18 08:33:32 -0600

    Comentar codigo

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ var app = express()
 var cf_app = require( './app/vcap_application')
 var cf_svc = require( './app/vcap_services')
 
-app.set( 'views', __dirname + '/views')
-app.set( 'view engine', 'jade')
-app.use( express.static( __dirname + '/public'))
+//app.set( 'views', __dirname + '/views')
+//app.set( 'view engine', 'jade')
+//app.use( express.static( __dirname + '/public'))
 
 app.get( '/', function ( req, res) {
-  res.render( 'pages/index', {
+ /* res.render( 'pages/index', {
     app_environment:    app.settings.env,
     application_name:   cf_app.get_app_uris(),
     app_uris:           cf_app.get_app_name(),
@@ -19,7 +19,7 @@ app.get( '/', function ( req, res) {
     service_label:      cf_svc.get_service_label(),
     service_name:       cf_svc.get_service_name(),
     service_plan:       cf_svc.get_service_plan()
-  })
+  })*/
   res.send('Welcome to Bluemix DevOps with Docker. Lets go use Pipeline Services. I am Nicolas from telefonica, nice to meet you!!' )
 })
 
