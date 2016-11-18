89cece1d8d251c3c159d1a95348e9a42ca5955d3
Juan Manuel Cruz <juan.cruzyanez@telefonica.com>
2016-11-18 08:17:53 -0600

    Cambio en punto y coma

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-var express = require('express');
+var express = require( 'express')
+var app = express()
+var cf_app = require( './app/vcap_application')
+var cf_svc = require( './app/vcap_services')
 
-var PORT = 80;
+app.set( 'views', __dirname + '/views')
+app.set( 'view engine', 'jade')
+app.use( express.static( __dirname + '/public'))
 
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Welcome to Bluemix DevOps with Docker. Lets go use Pipeline Services. I am Nicolas from telefonica, nice to meet you!' );
-});
+app.get( '/', function ( req, res) {
+  res.render( 'pages/index', {
+    app_environment:    app.settings.env,
+    application_name:   cf_app.get_app_name(),
+    app_uris:           cf_app.get_app_uris(),
+    app_space_name:     cf_app.get_app_space(),
+    app_index:          cf_app.get_app_index(),
+    app_mem_limits:     cf_app.get_app_mem_limits(),
+    app_disk_limits:    cf_app.get_app_disk_limits(),
+    service_label:      cf_svc.get_service_label(),
+    service_name:       cf_svc.get_service_name(),
+    service_plan:       cf_svc.get_service_plan()
+  })
+  res.send('Welcome to Bluemix DevOps with Docker. Lets go use Pipeline Services. I am Nicolas from telefonica, nice to meet you!!' )
+})
 
-app.listen(PORT)
-console.log(' Application Running on port' + PORT);
+app.listen( process.env.PORT || 4000)
